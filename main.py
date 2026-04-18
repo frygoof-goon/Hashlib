ca1c160a36cd77bc68175aa147bace379de97d6e
evg <frygoof@gmail.com>
2026-04-18 21:22:52 +0500

    first

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ def main():
     path = input("Введите путь к папке: ")
 
     init_db()
-
     files = scan_directory(path)
 
     total = 0
@@ -16,19 +15,25 @@ def main():
     safe = 0
     unknown = 0
 
+    local_found = 0
+    online_checked = 0
+
     for file in files:
         total += 1
         try:
             file_hash = calculate_sha256(file)
 
+            # --- ЛОКАЛЬНАЯ ПРОВЕРКА ---
             if check_local_db(file_hash):
                 status = "MALICIOUS"
                 source = "LOCAL"
                 malicious += 1
+                local_found += 1
 
             else:
                 status = check_online(file_hash)
                 source = "ONLINE"
+                online_checked += 1
 
                 if status == "MALICIOUS":
                     malicious += 1
@@ -51,6 +56,10 @@ def main():
     print("Вредоносных:", malicious)
     print("Безопасных:", safe)
     print("Неизвестных:", unknown)
+
+    print("\n=== ПО ИСТОЧНИКАМ ===")
+    print("Найдено локально:", local_found)
+    print("Проверено через API:", online_checked)
 
 
 if __name__ == "__main__":
